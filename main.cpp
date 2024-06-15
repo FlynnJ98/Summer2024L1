02a26d46315c356847515adb38123fecec8132dd
FlynnJ98 <jamesflynn360@gmail.com>
2024-06-15 10:16:44 +0000

    chnages

--- main.cpp
+++ main.cpp
@@ -1,10 +1,26 @@
 #include "mbed.h"
+#include <cstdio>
+
+// A few words, max 100 onthe program
+
+
+InterruptIn bluebtn(PC_13);
+volatile int int_trig = 0;
+
+void bttnFall(){
+    int_trig = 1;
+}
 
 // main() runs in its own thread in the OS
 int main()
 {
+    bluebtn.fall(&bttnFall);
+
+    //this is a program for lab 1 embedded systems module 
     while (true) {
-
-    }
-}
-
+        if(int_trig == 1){
+        int_trig = 0;
+        printf("bttnFall \r\n");
+        }
+        }
+        }
